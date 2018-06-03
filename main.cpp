e690733a6c82a1b2ff4a3c0c2a81773fb7789b02
Aayush Gupta <aayu.gupta.96@gmail.com>
2018-06-03 18:32:25 +0530

    Render normals

--- main.cpp
+++ main.cpp
@@ -2,26 +2,34 @@
 #include <cctype>
 #include "ray.h"
 
-bool hit_sphere(const vec3& center, float radius, const ray& r){
+float hit_sphere(const vec3& center, float radius, const ray& r){
 	vec3 oc = r.origin() - center;
 	float a = dot(r.direction(), r.direction());
 	float b = 2.0 * dot(oc, r.direction());
 	float c = dot(oc, oc) - radius*radius;
 	float discriminant = b*b - 4*a*c;
 
-	return (discriminant > 0);
+	if (discriminant < 0) {
+		return -1.0;
+	}
+	else {
+		return (-b - sqrt(discriminant)) / (2.0*a);
+	}
 }
 
 vec3 color(const ray& r) {
 	// Linear blending based on y coordinate
 
 	// If we hit the sphere at position (0,0,-1) then we return red color.
-	if (hit_sphere(vec3(0,0,-2), 0.5, r))
-		return vec3(1, 0, 0);
+	float t = hit_sphere(vec3(0,0,-1), 0.5, r);
+	if (t > 0.0) {
+		vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0,0,-1));
+		return 0.5*vec3(N.x()+1, N.y()+1, N.z()+1);
+	}
 	// Make it an unit vector so -1.0 < y < 1.0
 	vec3 unit_direction = unit_vector(r.direction());
 	// Scale y to t such that 0.0 < t < 1.0
-	float t = 0.5 * (unit_direction.y() + 1.0);
+	t = 0.5 * (unit_direction.y() + 1.0);
 	// Linear blend(lerp) where top is blue(0.5,0.7,1.0) and bottom is white
 	// Formula: blend_color = (1-t)*white + t*blue
 	return (1.0-t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
