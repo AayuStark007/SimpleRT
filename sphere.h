b324a8486e089708f7aeae444ec82b982c28b477
Aayush Gupta <aayu.gupta.96@gmail.com>
2018-06-03 22:56:52 +0530

    Fix formatting

--- sphere.h
+++ sphere.h
@@ -3,42 +3,47 @@
 
 #include "hitable.h"
 
-class sphere: public hitable {
+class sphere : public hitable
+{
 
-public:
+  public:
     sphere() {}
-    sphere(vec3 cen, float r) : center(cen), radius(r) {};
-    
-    virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const;
-    
+    sphere(vec3 cen, float r) : center(cen), radius(r){};
+
+    virtual bool hit(const ray &r, float t_min, float t_max, hit_record &rec) const;
+
     vec3 center;
     float radius;
 };
 
 // Removed unecessary 2 which cancel out eventually
-bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
+bool sphere::hit(const ray &r, float t_min, float t_max, hit_record &rec) const
+{
     vec3 oc = r.origin() - center;
-	float a = dot(r.direction(), r.direction());
-	float b = dot(oc, r.direction());
-	float c = dot(oc, oc) - radius*radius;
-	float discriminant = b*b - a*c;
-	if (discriminant > 0) {
-		float temp = (-b - sqrt(b*b - a*c)) / a;
-        if (temp < t_max && temp > t_min) {
+    float a = dot(r.direction(), r.direction());
+    float b = dot(oc, r.direction());
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b * b - a * c;
+    if (discriminant > 0)
+    {
+        float temp = (-b - sqrt(b * b - a * c)) / a;
+        if (temp < t_max && temp > t_min)
+        {
             rec.t = temp;
             rec.p = r.point_at_parameter(rec.t);
             rec.normal = (rec.p - center) / radius;
             return true;
         }
-        temp = (-b + sqrt(b*b - a*c)) / a;
-        if (temp < t_max && temp > t_min) {
+        temp = (-b + sqrt(b * b - a * c)) / a;
+        if (temp < t_max && temp > t_min)
+        {
             rec.t = temp;
             rec.p = r.point_at_parameter(rec.t);
             rec.normal = (rec.p - center) / radius;
             return true;
         }
-	}
-	return false;
+    }
+    return false;
 }
 
 #endif
