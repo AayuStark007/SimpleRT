977c115b1f615f65ff151431d997f214b6617c0e
Aayush Gupta <aayu.gupta.96@gmail.com>
2018-11-07 11:09:06 +0530

    add positionable camera

--- main.cpp
+++ main.cpp
@@ -52,6 +52,7 @@ int main(int argc, char *argv[])
 	std::cout << "P3\n"
 			  << nx << " " << ny << "\n255\n";
 
+	// TODO: Build config file for objects to be added to scene.
 	hitable *list[5];
 	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
 	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
@@ -60,7 +61,7 @@ int main(int argc, char *argv[])
 	list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
 	hitable *world = new hitable_list(list, 5);
 
-	camera cam;
+	camera cam(vec3(-2, 2, 1), vec3(0, 0, -1), vec3(0, 1, 0), 20, float(nx)/float(ny));
 
 	for (int j = ny - 1; j >= 0; j--)
 	{
