e083290416082f0eaaa155ad383ab6cac2a5a1f1
Aayush Gupta <aayu.gupta.96@gmail.com>
2018-11-07 13:06:53 +0530

    code to render final scene

--- main.cpp
+++ main.cpp
@@ -38,6 +38,36 @@ vec3 color(const ray &r, hitable *world, int depth)
 	}
 }
 
+hitable *random_scene(int n)
+{
+	hitable **list = new hitable*[n+1];
+	list[0] = new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5,0.5,0.5)));
+	int i = 1;
+	for (int a = -11; a < 11; a++) {
+		for (int b = -11; b < 11; b++) {
+			float choose_mat = drand48();
+			vec3 center(a+0.9*drand48(), 0.2, b+0.9*drand48());
+			if ((center-vec3(4,0.2,0)).length() > 0.9) {
+				if (choose_mat < 0.8) { //diffuse
+					list[i++] = new sphere(center, 0.2, new lambertian(vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48())));
+				}
+				else if (choose_mat < 0.95) { //metal
+					list[i++] = new sphere(center, 0.2, 
+						new metal(vec3(0.5*(1 + drand48()), 0.5*(1 + drand48()), 0.5*(1 + drand48())), 0.5*drand48()));
+				}
+				else { //glass
+					list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+				}
+			}
+		}
+	}
+	list[i++] = new sphere(vec3( 0, 1, 0), 1.0, new dielectric(1.5));
+	list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+	list[i++] = new sphere(vec3( 4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+	return new hitable_list(list, i);
+}
+
 int main(int argc, char *argv[])
 {
 	int nx = 200;
@@ -47,21 +77,22 @@ int main(int argc, char *argv[])
 	{
 		nx = atoi(argv[1]);
 		ny = atoi(argv[2]);
+		ns = atoi(argv[3]);
 	}
 
 	std::cout << "P3\n"
 			  << nx << " " << ny << "\n255\n";
 
 	// TODO: Build config file for objects to be added to scene.
-	hitable *list[5];
-	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
-	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.3));
-	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
-	list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
-	hitable *world = new hitable_list(list, 5);
+	
+	hitable *world = random_scene(500);
 
-	camera cam(vec3(-2, 2, 1), vec3(0, 0, -1), vec3(0, 1, 0), 20, float(nx)/float(ny));
+	vec3 lookfrom(13,2,3);
+	vec3 lookat(0,0,0);
+	float dist_to_focus = 10.0; //(lookfrom-lookat).length();
+	float aperture = 0.1;
+
+	camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, float(nx)/float(ny), aperture, dist_to_focus);
 
 	for (int j = ny - 1; j >= 0; j--)
 	{
@@ -84,7 +115,6 @@ int main(int argc, char *argv[])
 			int ib = int(255.99 * col[2]);
 
 			std::cout << ir << " " << ig << " " << ib << "\n";
-			//std::cout << "i: " << i << " j: " << j << "\n";
 		}
 	}
 }
